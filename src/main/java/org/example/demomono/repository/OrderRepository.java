aac0054f70df22f29c976a06171c9c33514251d3
mohamed <mohamed@utopios.net>
2024-10-18 12:32:53 +0200

    fix error

--- src/main/java/org/example/demomono/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.demomono.repository;
-
-import org.example.demomono.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- src/main/java/org/example/demomono/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.demomono.repository;
-
-import org.example.demomono.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
