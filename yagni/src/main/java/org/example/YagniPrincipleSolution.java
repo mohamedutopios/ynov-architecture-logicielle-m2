fa8a3dc9428054183c6bc79419face6113f4784c
mohamed <mohamed@utopios.net>
2024-11-07 11:41:49 +0100

    add solution yagni

--- yagni/src/main/java/org/example/YagniPrincipleSolution.java
+++ yagni/src/main/java/org/example/YagniPrincipleSolution.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class YagniPrincipleSolution {
+
+    private List<String> messages = new ArrayList<>();
+
+
+    public void sendMessage(String message) {
+        messages.add(message);
+        System.out.println("Message sent: " + message);
+    }
+
+    public void receiveMessage() {
+        if (!messages.isEmpty()) {
+            String message = messages.remove(0);
+            System.out.println("Message received: " + message);
+        } else {
+            System.out.println("No messages to receive.");
+        }
+    }
+
+
 }
