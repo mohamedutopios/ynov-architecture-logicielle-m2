9e349710e91a72110297f1574a9716b0c0f8be0d
mohamed <mohamed@utopios.net>
2024-11-08 14:48:03 +0100

    add demo adapter

---
+++ design-pattern/src/main/java/org/example/structural/adapter/PlugAdapter.java
@@ -0,0 +1,16 @@
+package org.example.structural.adapter;
+
+public class PlugAdapter implements EuropeanPlug{
+
+    private AmericanPlug americanPlug;
+
+    public PlugAdapter(AmericanPlug americanPlug) {
+        this.americanPlug = americanPlug;
+    }
+
+    @Override
+    public void connectToEuropeanPlug() {
+        System.out.println("Adapteur en cours ...");
+        americanPlug.connectToAmericanPlug();
+    }
+}
