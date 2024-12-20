3bfdad46ca363b0a9e40631025c1e231ac3e8b93
mohamed <mohamed@gmail.com>
2024-12-20 11:46:36 +0100

    add kafkaProducerAccount

---
+++ micro-archi/account-service/src/main/java/org/example/accountservice/kafka/AccountKafkaProducer.java
@@ -0,0 +1,29 @@
+package org.example.accountservice.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountKafkaProducer {
+    private static final Logger logger = LoggerFactory.getLogger(AccountKafkaProducer.class);
+    private final KafkaTemplate<String,String > kafkaTemplate;
+
+    @Value("${spring.kafka.topic.account:account-events}")
+    private String topic;
+
+    public AccountKafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendAccountDeleteEvent(Long accountId){
+        String event = String.format("{\"event\":\"ACCOUNT_DELETED\",\"accountId\":\"%s\"}", accountId);
+
+    }
+
+
+
+
+}
